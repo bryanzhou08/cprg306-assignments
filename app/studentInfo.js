bbf79f3fc963a19f2d97fca387a255a39068f4b1
bryanzhou08 <bryanzhou08@gmail.com>
2023-09-21 10:44:51 -0600

    added week2

---
+++ app/studentInfo.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Bryan</p>
+            <p>cprg306D</p>
+            <Link href="https://github.com/bryanzhou08">https://github.com/bryanzhou08</Link>
+        </div>
+    );
+}
---
+++ app/week2/page.js
@@ -0,0 +1,9 @@
+import StudentInfo from "../studentInfo";
+export default function Home() {
+    return (
+        <div>
+           <h1>My Shopping List</h1> 
+           <StudentInfo />
+        </div>
+    );
+}
